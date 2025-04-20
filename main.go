58a1d338f4038242af988051c8b61f3bab4d7522
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-20 10:04:15 +0100

    added jsonand made a function to convert json to struct

--- main.go
+++ main.go
@@ -1,6 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"log"
+	"encoding/json"
+)
+
+type uniRequest struct {
+	short string `json:"abbreviation"`
+}
+
+func convertUniStruct() []uniRequest {
+
+	uni,err:= os.ReadFile("/Users/user/go/src/9jauni/json/uni.json")
+	if err != nil {
+		log.Fatalf("an error occured: %v", err)
+	}
+	// fmt.Println(string(uni))
+
+	var uniRequests []uniRequest
+    if err := json.Unmarshal(uni, &uniRequests); err != nil {
+        log.Fatalf("failed to unmarshal JSON: %v", err)
+    }
+
+
+	
+	return uniRequests
+
+}
+
+
+
+
+func getAllUni(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getAUni(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
+	convertUniStruct();
 	fmt.Println("hello")
+	http.HandleFunc("/",getAllUni)
+	http.HandleFunc("/search",getAUni)
+	http.ListenAndServe(":8080", nil);
+	fmt.Println("Server started at :8080")
 }
