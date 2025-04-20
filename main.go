c521fd78dcc9cd848bb8024c3cb8a7138905b001
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-20 13:48:47 +0100

    make somechanges to search by abbreviation

--- main.go
+++ main.go
@@ -9,60 +9,52 @@ import (
 )
 
 type uniRequest struct {
-	
-    Name        string `json:"name"`
-    Abbreviation  string `json:"abbreviation"`
-    WebsiteLink   string `json:"website_link"`
+	Name         string `json:"name"`
+	Abbreviation string `json:"abbreviation"`
+	WebsiteLink  string `json:"website_link"`
 }
 type uniARequest struct {
-	
-    Name        string `json:"name"`
-    Abbreviation  string `json:"abbreviation"`
-    WebsiteLink   string `json:"website_link"`
+	Name         string `json:"name"`
+	Abbreviation string `json:"abbreviation"`
+	WebsiteLink  string `json:"website_link"`
+}
+type Abbreviation struct{
+	Abbreviation string `json:"abbreviation"`
+
 }
 
 func convertUniStruct() []uniRequest {
 
-	uni,err:= os.ReadFile("/Users/user/go/src/9jauni/json/uni.json")
+	uni, err := os.ReadFile("/Users/user/go/src/9jauni/json/uni.json")
 	if err != nil {
 		log.Fatalf("an error occured: %v", err)
 	}
 	// fmt.Println(string(uni))
 
 	var uniRequests []uniRequest
-    if err := json.Unmarshal(uni, &uniRequests); err != nil {
-        log.Fatalf("failed to unmarshal JSON: %v", err)
-    }
-	
-	
+	if err := json.Unmarshal(uni, &uniRequests); err != nil {
+		log.Fatalf("failed to unmarshal JSON: %v", err)
+	}
+
 	return uniRequests
 
 }
 
-
-
-
-func getAllUni(w http.ResponseWriter, r *http.Request)  {
+func getAllUni(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-result:=convertUniStruct();
+	result := convertUniStruct()
 
-// Set the response header to indicate JSON content
-w.Header().Set("Content-Type", "application/json")
+	// Set the response header to indicate JSON content
+	w.Header().Set("Content-Type", "application/json")
 
-// Encode the result directly to the response writer
-if err := json.NewEncoder(w).Encode(result); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
+	// Encode the result directly to the response writer
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
-}
-
-
-
-
-
-
 
 func getAUni(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
@@ -71,39 +63,64 @@ func getAUni(w http.ResponseWriter, r *http.Request) {
 	}
 	decoder := json.NewDecoder(r.Body)
 
-	var myUni uniARequest;
-
-
+	var myUni uniARequest
 
 	decoder = json.NewDecoder(r.Body)
 	if err := decoder.Decode(&myUni); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	 fmt.Println(myUni)
-	 for _, uni := range convertUniStruct() {
+	fmt.Println(myUni)
+	for _, uni := range convertUniStruct() {
 		if uni.Name == myUni.Name {
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(uni)
 			return
-		}else{
-			http.Error(w, "University not found", http.StatusNotFound)
+		}
+		// else{
+		// 	http.Error(w, "University not found", http.StatusNotFound)
+		// 	return
+		// }
+
+	}
+
+}
+func getAUniByAB(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	var myUni uniARequest
+
+	decoder = json.NewDecoder(r.Body)
+	if err := decoder.Decode(&myUni); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println(myUni)
+	for _, uni := range convertUniStruct() {
+		if uni.Abbreviation == myUni.Abbreviation{
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(uni)
 			return
 		}
+		// else{
+		// 	http.Error(w, "University not found", http.StatusNotFound)
+		// 	return
+		// }
 
+	}
 
-
-
-	 }
-	
-	
 }
 
 func main() {
-	convertUniStruct();
+	convertUniStruct()
 	fmt.Println("hello")
-	http.HandleFunc("/",getAllUni)
-	http.HandleFunc("/search",getAUni)
-	http.ListenAndServe(":8080", nil);
+	http.HandleFunc("/", getAllUni)
+	http.HandleFunc("/search", getAUni)
+	http.HandleFunc("/searchab", getAUniByAB)
+	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server started at :8080")
 }
