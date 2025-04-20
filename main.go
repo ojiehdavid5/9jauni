5c6c875021c4f68779220ed12ed2218be198190b
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-20 13:18:49 +0100

    done continue with using abbreviation

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 type uniRequest struct {
-	short string `json:"abbreviation"`
+	
+    Name        string `json:"name"`
+    Abbreviation  string `json:"abbreviation"`
+    WebsiteLink   string `json:"website_link"`
+}
+type uniARequest struct {
+	
+    Name        string `json:"name"`
+    Abbreviation  string `json:"abbreviation"`
+    WebsiteLink   string `json:"website_link"`
 }
 
 func convertUniStruct() []uniRequest {
@@ -24,8 +33,7 @@ func convertUniStruct() []uniRequest {
     if err := json.Unmarshal(uni, &uniRequests); err != nil {
         log.Fatalf("failed to unmarshal JSON: %v", err)
     }
-
-
+	
 	
 	return uniRequests
 
@@ -34,12 +42,61 @@ func convertUniStruct() []uniRequest {
 
 
 
-func getAllUni(w http.ResponseWriter, r *http.Request) {
+func getAllUni(w http.ResponseWriter, r *http.Request)  {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+result:=convertUniStruct();
 
+// Set the response header to indicate JSON content
+w.Header().Set("Content-Type", "application/json")
+
+// Encode the result directly to the response writer
+if err := json.NewEncoder(w).Encode(result); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+}
 }
 
-func getAUni(w http.ResponseWriter, r *http.Request) {
 
+
+
+
+
+
+func getAUni(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	var myUni uniARequest;
+
+
+
+	decoder = json.NewDecoder(r.Body)
+	if err := decoder.Decode(&myUni); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	 fmt.Println(myUni)
+	 for _, uni := range convertUniStruct() {
+		if uni.Name == myUni.Name {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(uni)
+			return
+		}else{
+			http.Error(w, "University not found", http.StatusNotFound)
+			return
+		}
+
+
+
+
+	 }
+	
+	
 }
 
 func main() {
